513af197374459969d22c07cfb1becb5c99f7d5d
Diego <di.allende@duocuc.cl>
2023-06-20 14:43:20 -0400

    commit Crud

--- prueba_ground/obras/views.py
+++ prueba_ground/obras/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import get_object_or_404, render, redirect
+
+from obras.form import CreateObraForm
 from .models import Obras
 import os
 
@@ -7,34 +9,15 @@ def obras(request):
     return render(request, "obras/obras.html", {'obras': obras})
 
 def crear_elemento(request):
-
     if request.method == 'POST':
-        nombre= request.POST["nombre"]
-        precio= request.POST["Precio"]
-        nombre_autor= request.POST["nombre_autor"]
-        titulo= request.POST["titulo"]
-        tecnica= request.POST["tecnica"]
-        anio= request.POST["anio"]
-        detalle= request.POST["detalle"]
-        imagen=request.POST["imagen"]
-        #imagen = imagen.replace(" ","_")
-        imagen_ruta = os.path.join("Services/", imagen)
-        
-        obj=Obras.objects.create(
-            Nombre=nombre,
-            Precio = precio,
-            Autor = nombre_autor,
-            Titulo_Original= titulo,
-            Tecnica = tecnica,
-            Anio = anio,
-            Detalles_obra = detalle,
-            image = imagen_ruta
-            )
-        obj.save()
-        context={'mensaje':"OK, datos grabados..."}
+        form = CreateObraForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('obras')
     else:
-        return render(request, 'obras/create_obra.html')
-    return render(request, 'obras/create_obra.html', context)
+        form = CreateObraForm()
+
+    return render(request, 'obras/create_obra.html', {'form': form})
 
 def detalle_obra(request, pk):
     obra = get_object_or_404(Obras, pk=pk)

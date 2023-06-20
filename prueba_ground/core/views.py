90840e39f13c2158acbf9b45f710c04c68da22db
Diego <di.allende@duocuc.cl>
2023-06-20 16:10:55 -0400

    v3

--- prueba_ground/core/views.py
+++ prueba_ground/core/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import get_object_or_404, render
+from django.views.generic.list import ListView
 from obras.models import Obras
-from django.views.generic import ListView
-
 
 # Create your views here.
 def index(request):
-    return render(request, "core/home.html")
+    obras = Obras.objects.all()[:3]  # Obtén las primeras 3 obras
+    return render(request, "core/home.html", {'obras': obras})
 
 def carrito(request):
     return render(request, "core/carrito.html")
@@ -13,3 +13,8 @@ def carrito(request):
 def detalles_obra(request, pk):
     obra = get_object_or_404(Obras, pk=pk)
     return render(request, 'obras/detalles_obra.html', {'obra': obra})
+
+class ObrasListView(ListView):
+    model = Obras
+    template_name = 'core/home.html'
+    context_object_name = 'obras'
